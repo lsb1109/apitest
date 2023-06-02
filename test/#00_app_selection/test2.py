3548a73312fcc7227b3808e7f70626ceda7a4da4
lsb1109 <lsb1109@midasit.com>
2023-06-02 17:59:31 +0900

    tet7

---
+++ test/#00_app_selection/test2.py
@@ -0,0 +1,19 @@
+q = Queue()
+p = multiprocessing.Process(target=sub_process_code, args=(q, ))
+p.start()
+selected_data = q.get()
+
+contents_name = selected_data[1][selected_data[2]]
+
+if selected_data[0] == 0:
+    second_code = extract_code(
+        main_link + str(contents_name) + py_code_file)
+    exec(second_code)
+
+elif selected_data[0] == 1:
+    excel = win32.Dispatch("Excel.Application")
+    excel.Visible = True
+    workbook = excel.Workbooks.Open(local_xl_path + contents_name)
+
+else:
+    pass
